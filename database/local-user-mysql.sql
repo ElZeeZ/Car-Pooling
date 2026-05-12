31e040bdc367089d7f370a4e64b7feaa216f2da5
Zein Abed Ali <109868609+ElZeeZ@users.noreply.github.com>
2026-05-12 14:14:22 +0300

    Delete database/local-user-mysql.sql

--- database/local-user-mysql.sql
+++
@@ -1,6 +0,0 @@
-CREATE USER IF NOT EXISTS 'carpool_app'@'localhost'
-  IDENTIFIED WITH mysql_native_password BY 'carpool_password';
-
-GRANT ALL PRIVILEGES ON carpooling_db.* TO 'carpool_app'@'localhost';
-
-FLUSH PRIVILEGES;
