e72d925684bb8fac1a03110c00edad313d693bd5
Zein Abed Ali <109868609+ElZeeZ@users.noreply.github.com>
2026-05-12 14:14:14 +0300

    Delete database/local-user.sql

--- database/local-user.sql
+++
@@ -1,6 +0,0 @@
-CREATE USER IF NOT EXISTS 'carpool_app'@'localhost'
-  IDENTIFIED BY 'carpool_password';
-
-GRANT ALL PRIVILEGES ON carpooling_db.* TO 'carpool_app'@'localhost';
-
-FLUSH PRIVILEGES;
